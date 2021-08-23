16611706f3ed1047e98256b443c0a59f4f20109a
Brian Feng <brianfeng95@aim.com>
2021-08-23 19:05:43 -0400

    initialize python syntax exercise

---
+++ python-syntax/words.py
@@ -0,0 +1,22 @@
+def print_upper_words(str, must_start_with=None):
+    """
+    Prints out each word on a separate line, but in all uppercase
+    
+    The first argument is the list of words to potentially print.
+
+    The second argument is the list of letters such that this will only print words that start with one of those letters.
+    If the second argument is not entered, this will print all words in uppercase.
+    """
+
+    if must_start_with == None:
+        for word in str:
+            print(word.upper())
+        return
+
+    for word in str:
+        for char in must_start_with:
+            if word[0].lower() == char:
+              print(word.upper())
+
+# this should print "HELLO", "HEY", "YO", and "YES"
+print_upper_words(["hello", "hey", "goodbye", "yo", "yes"], must_start_with=["h", "y"])
