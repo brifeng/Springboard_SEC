13d78fac96fd82f7111c438078d6f6a8671c499d
Brian Feng <brianfeng95@aim.com>
2021-10-27 18:56:54 -0400

    include step 7 of exercise

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -11,6 +11,7 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
+    """Show Boggle game board"""
     session['board'] = boggle_game.make_board()
     if not 'times-played' in session:
         session['times-played'] = 0
@@ -24,17 +25,20 @@ def home():
 
 @app.route('/check-word/<word>')
 def check_word(word):
+    """Check if word is valid (on game board and is listed in dictionary file"""
     return boggle_game.check_valid_word(session['board'], word)
 
 
 @app.route('/start-game')
 def start():
+    """Increments the number of times played in session"""
     session['times-played'] = session['times-played'] + 1
     return jsonify({'timesPlayed': session['times-played']})
 
 
 @app.route('/update-high-score/<int:high>')
 def update_high_score(high):
+    """Update the high score in session if new score is higher"""
     if session['high-score'] < high:
         session['high-score'] = high
     return jsonify({'highScore': session['high-score']})
--- flask-boggle/static/app.js
+++ flask-boggle/static/app.js
@@ -1,69 +1,86 @@
-let score = 0;
-let timer = 60;
+class BoggleGame {
+    constructor(time) {
+        this.timer = time;
+        this.score = 0;
+        this.wordBank = [];
 
-$('#guess-form').on('submit', async function guess(evt) {
-    evt.preventDefault();
 
-    word = $('#guess-word')[0].value;
-    handleGuess(word);
+        $('#guess-form').on('submit', function guess(evt) {
+            evt.preventDefault();
 
-    $('#guess-word')[0].value = '';
-})
+            const word = $('#guess-word')[0].value;
+            game.handleGuess(word);
 
-$('#start').on('click', () => {
-    startGame();
-    setTimeout(() => {
-        endGame();
-    }, timer*1000);
-})
+            $('#guess-word')[0].value = '';
+        })
 
-async function handleGuess(word) {
-    const res = await axios.get(`http://localhost:5000/check-word/${word}`);
-    alert(res.data);
+        $('#start').on('click', () => {
+            this.startGame();
+            setTimeout(() => {
+                this.endGame();
+            }, this.timer * 1000);
+        })
+    }
 
-    if (res.data === 'ok') {
-        changeScore(word.length)
+
+    async handleGuess(word) {
+        if (this.wordBank.indexOf(word) === -1) {
+            this.wordBank.push(word);
+        } else {
+            alert('already used');
+            return
+        }
+        const res = await axios.get(`http://localhost:5000/check-word/${word}`);
+        alert(res.data);
+
+        if (res.data === 'ok') {
+            this.changeScore(word.length)
+        }
+    }
+
+    changeScore(add) {
+        game.score = game.score + add;
+
+        let scoreHTML = $('#score');
+        scoreHTML[0].innerHTML = `Score: ${this.score}`;
+    }
+
+    changeTimer(time) {
+        let timerHTML = $('#timer');
+        timerHTML[0].innerHTML = `Time Left: ${time}`
+    }
+
+    startGame() {
+        $('#guess-form')[0].hidden = false;
+        $('#timer')[0].hidden = false;
+
+        setInterval(function () {
+            game.timer = game.timer - 1;
+            game.changeTimer(game.timer);
+        }, 1000);
+    }
+
+    endGame() {
+        alert('GAME OVER!');
+        $('#guess-form')[0].hidden = true;
+        $('#timer')[0].hidden = true;
+        this.updateTimesPlayed();
+        this.updateHighScore(this.score);
+    }
+
+    async updateTimesPlayed() {
+        const res = await axios.get(`http://localhost:5000/start-game`);
+        const times = res.data.timesPlayed;
+        $('#play-count')[0].innerHTML = `Times Played: ${times}`;
+    }
+
+    async updateHighScore(highScore) {
+        const res = await axios.get(`http://localhost:5000/update-high-score/${highScore}`);
+        const high = res.data.highScore;
+        $('#high-score')[0].innerHTML = `High Score: ${high}`
     }
 }
 
-function changeScore(add) {
-    score = score + add;
+let game = new BoggleGame(60);
 
-    let scoreHTML = $('#score');
-    scoreHTML[0].innerHTML = `Score: ${score}`;
-}
 
-function changeTimer(time) {
-    let timerHTML = $('#timer');
-    timerHTML[0].innerHTML = `Time Left: ${time}`
-}
-
-async function startGame() {
-    $('#guess-form')[0].hidden = false;
-    $('#timer')[0].hidden = false;
-
-    setInterval(function () {
-        timer = timer - 1;
-        changeTimer(timer);
-    }, 1000);
-}
-
-function endGame() {
-    alert('GAME OVER!');
-    $('#guess-form')[0].hidden = true;
-    $('#timer')[0].hidden = true;
-    updateTimesPlayed();
-    updateHighScore(score);
-}
-
-async function updateTimesPlayed() {
-    const res = await axios.get(`http://localhost:5000/start-game`);
-    const times = res.data.timesPlayed;
-    $('#play-count')[0].innerHTML = `Times Played: ${times}`;
-}
-
-async function updateHighScore(highScore) {
-    const res = await axios.get(`http://localhost:5000/update-high-score/${highScore}`);
-    const high = res.data.highScore;
-    $('#high-score')[0].innerHTML = `High Score: ${high}`
-}
